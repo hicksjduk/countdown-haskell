64bf83d1cfc41b311ccb09b76c34e793636fe213
Jeremy Hicks <jerhicks@synamedia.com>
2022-01-19 14:26:07 +0000

    Command-line interface

--- app/Main.hs
+++ app/Main.hs
@@ -6,9 +6,50 @@ import System.Environment
 
 main = do
    args <- getArgs
-   if and [all isDigit arg | arg <- args] then solveIt $ [read a::Int | a <- args]
-   else putStrLn "Must only specify numbers"
+   case validateAndConvert args of
+      (Message m) -> putStrLn m
+      (Numbers n) -> solveIt n
 
 solveIt (target:numbers)  = do
     putStrLn (join " " ["Solving with target number:", show target, "and source numbers:", show numbers])
     putStrLn (show $ Solution(solve target numbers))
+
+data ValidationResult = Message String | Numbers [Int]
+
+instance Show ValidationResult where
+   show (Message m) = m
+   
+valid :: ValidationResult -> Bool
+valid (Message _) = False
+valid _ = True
+
+validateAndConvert :: [String] -> ValidationResult
+validateAndConvert [] = Message "Must specify at least one argument"
+validateAndConvert args
+   | or [any (not.isDigit) x | x <- args] = Message "All arguments must be numeric"
+   | otherwise = validateNumbers [read x::Int | x <- args]
+   
+validateNumbers :: [Int] -> ValidationResult
+validateNumbers (x:[]) = Message "One argument specified, not supported yet"
+validateNumbers nums@(target:numbers)
+   | (not.validTarget) target = Message "Target must be in the range 100 to 999 inclusive"
+   | (not.validNumbers) numbers = Message "Numbers must include up to 2 each of the numbers 1 to 10 and up to 1 each of 25, 50, 75 and 100"
+   | otherwise = Numbers nums
+   
+validTarget :: Int -> Bool
+validTarget n = n >= 100 && n <= 999
+
+validNumbers :: [Int] -> Bool
+validNumbers ns = and [validNumber n ns | n <- ns]
+
+validNumber :: Int -> [Int] -> Bool
+validNumber n ns
+   | n < 1 = False
+   | n > 100 = False
+   | n <= 10 = occurrences n ns <= 2
+   | n `mod` 25 == 0 = occurrences n ns == 1
+   | otherwise = False
+   
+occurrences :: Eq a => a -> [a] -> Int
+occurrences _ [] = 0
+occurrences y (x:xs) = (if y == x then 1 else 0) + occurrences y xs
