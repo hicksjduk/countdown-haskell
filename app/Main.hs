aa2ab91fd69c73a139086ea1698974ac9e0969a3
Jeremy Hicks <jerhicks@synamedia.com>
2022-01-21 11:51:20 +0000

    Reformat and take hints

--- app/Main.hs
+++ app/Main.hs
@@ -1,47 +1,49 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
 module Main where
 
 import Countdown
-import Utils
 import Data.Char
 import System.Environment
 import System.Random
+import Utils
 
 main = do
-   args <- getArgs
-   rand <- getStdGen
-   case validateArgs args of
+  args <- getArgs
+  rand <- getStdGen
+  case validateArgs args of
+    (Just m) -> putStrLn m
+    _ -> case validateNumbers nums of
       (Just m) -> putStrLn m
-      _ -> case validateNumbers nums of
-              (Just m) -> putStrLn m
-              _ -> solveIt $ normalise nums rand
-         where nums = [read a::Int | a <- args]
+      _ -> solveIt $ normalise nums rand
+      where
+        nums = [read a :: Int | a <- args]
 
 normalise :: RandomGen a => [Int] -> a -> [Int]
 normalise [bigNums] rand = randomNumbers rand bigNums
 normalise nums _ = nums
 
 solveIt :: [Int] -> IO ()
-solveIt (target:numbers) = do
-    putStrLn (join " " ["Solving with target number:", show target, "and source numbers:", show numbers])
-    case solve target numbers of
-        (Just e) -> putStrLn $ join " = " [show e, show (value e)]
-        _ -> putStrLn "No solution found"
+solveIt (target : numbers) = do
+  putStrLn (join " " ["Solving with target number:", show target, "and source numbers:", show numbers])
+  case solve target numbers of
+    (Just e) -> putStrLn $ join " = " [show e, show (value e)]
+    _ -> putStrLn "No solution found"
 
 validateArgs :: [String] -> Maybe String
 validateArgs [] = Just "Must specify at least one argument"
 validateArgs xs
-   | all (all isDigit) xs = Nothing
-   | otherwise = Just "All arguments must be numeric"
+  | all (all isDigit) xs = Nothing
+  | otherwise = Just "All arguments must be numeric"
 
 validateNumbers :: [Int] -> Maybe String
-validateNumbers (x:[])
-   | x < 0 || x > 4 = Just "Number of big numbers must be in the range 0 to 4"
-   | otherwise = Nothing
-validateNumbers nums@(target:numbers)
-   | (not.validTarget) target = Just "Target must be in the range 100 to 999 inclusive"
-   | (not.validNumbers) numbers = Just "Numbers must include up to 2 each of the numbers 1 to 10 and up to 1 each of 25, 50, 75 and 100"
-   | otherwise = Nothing
+validateNumbers [x]
+  | x < 0 || x > 4 = Just "Number of big numbers must be in the range 0 to 4"
+  | otherwise = Nothing
+validateNumbers nums@(target : numbers)
+  | (not . validTarget) target = Just "Target must be in the range 100 to 999 inclusive"
+  | (not . validNumbers) numbers = Just "Numbers must include up to 2 each of the numbers 1 to 10 and up to 1 each of 25, 50, 75 and 100"
+  | otherwise = Nothing
 
 validTarget :: Int -> Bool
 validTarget n = n >= 100 && n <= 999
@@ -51,19 +53,23 @@ validNumbers ns = all (`validNumber` ns) $ distinct ns
 
 validNumber :: Int -> [Int] -> Bool
 validNumber n ns
-   | n < 1 = False
-   | n > 100 = False
-   | n <= 10 = occurrences n ns <= 2
-   | n `mod` 25 == 0 = occurrences n ns == 1
-   | otherwise = False
+  | n < 1 = False
+  | n > 100 = False
+  | n <= 10 = occurrences n ns <= 2
+  | n `mod` 25 == 0 = occurrences n ns == 1
+  | otherwise = False
 
 randomNumbers :: RandomGen a => a -> Int -> [Int]
-randomNumbers rand bigOnes = concat [take 1 (randomRs (100, 999) rand),
-                                     take bigOnes (randomFrom rand bigNumbers),
-                                     take (6-bigOnes) (randomFrom rand smallNumbers)]
-   where bigNumbers = [n * 25 | n <- [1..4]]
-         smallNumbers = concat [[n,n] | n <- [1..10]]
+randomNumbers rand bigOnes =
+  concat
+    [ take 1 (randomRs (100, 999) rand),
+      take bigOnes (randomFrom rand bigNumbers),
+      take (6 - bigOnes) (randomFrom rand smallNumbers)
+    ]
+  where
+    bigNumbers = [n * 25 | n <- [1 .. 4]]
+    smallNumbers = concat [[n, n] | n <- [1 .. 10]]
 
 randomFrom :: RandomGen a => a -> [Int] -> [Int]
 randomFrom rand [] = []
-randomFrom rand xs = take (length xs) [xs!!i | i <- distinct (randomRs (0, length xs - 1) rand)]
+randomFrom rand xs = take (length xs) [xs !! i | i <- distinct (randomRs (0, length xs - 1) rand)]
--- src/Countdown.hs
+++ src/Countdown.hs
@@ -1,102 +1,107 @@
 module Countdown where
 
-import Utils
 import Control.Parallel
 import Data.Maybe
-   
+import Utils
+
 solve :: Int -> [Int] -> Maybe Expression
 solve target [] = Nothing
-solve target xs = parallelFold (findBest target) $ map (Just) $ allExpressions $ map (NumberExpression) xs
+solve target xs = parallelFold (findBest target) $ map Just $ allExpressions $ map NumberExpression xs
 
 allExpressions :: [Expression] -> [Expression]
-allExpressions xs = concatMap (expressions) $ permute xs
+allExpressions xs = concatMap expressions $ permute xs
 
 expressions :: [Expression] -> [Expression]
 expressions [] = []
-expressions (x:[]) = [x]
-expressions xs = concatMap (expressionsFrom.(`splitAt` xs)) $ take (length xs - 1) [1..]
-   where
-      expressionsFrom :: ([Expression], [Expression]) -> [Expression]
-      expressionsFrom (leftOperands, rightOperands) = concat [combinations combiners r | r <- expressions rightOperands]
-         where combiners = concatMap (combinersUsing) (expressions leftOperands)
-
+expressions [x] = [x]
+expressions xs = concatMap (expressionsFrom . (`splitAt` xs)) $ take (length xs - 1) [1 ..]
+  where
+    expressionsFrom :: ([Expression], [Expression]) -> [Expression]
+    expressionsFrom (leftOperands, rightOperands) = concat [combinations combiners r | r <- expressions rightOperands]
+      where
+        combiners = concatMap combinersUsing (expressions leftOperands)
 
 permute :: Eq a => [a] -> [[a]]
 permute [] = []
-permute (x:[]) = [[x]]
+permute [x] = [[x]]
 permute xs = concat [permuteAt i xs | i <- uniqueIndices]
-   where uniqueIndices = distinctBy (xs!!) $ take (length xs) [0..]
-         permuteAt :: Eq a => Int -> [a] -> [[a]]
-         permuteAt n xs = [x : suffix | suffix <- [] : permute others]
-            where x = xs!!n
-                  others = allExcept n xs
+  where
+    uniqueIndices = distinctBy (xs !!) $ take (length xs) [0 ..]
+    permuteAt :: Eq a => Int -> [a] -> [[a]]
+    permuteAt n xs = [x : suffix | suffix <- [] : permute others]
+      where
+        x = xs !! n
+        others = allExcept n xs
 
+combinersUsing :: Expression -> [Expression -> Maybe Expression]
+combinersUsing left = [fromJust c | c <- filter isJust [combinerUsing op left | op <- [Add ..]]]
 
-combinersUsing :: Expression -> [(Expression -> Maybe Expression)]
-combinersUsing left = [fromJust c | c <- filter (isJust) [combinerUsing op left | op <- [Add ..]]]
-
-combinations :: [(Expression -> Maybe Expression)] -> Expression -> [Expression]
-combinations xs right = map (fromJust) (filter (isJust) [c right | c <- xs])
+combinations :: [Expression -> Maybe Expression] -> Expression -> [Expression]
+combinations xs right = catMaybes ([c right | c <- xs])
 
 makeExpression :: Operation -> Expression -> Expression -> Maybe Expression
 makeExpression op@Add left right = Just (ArithmeticExpression left op right)
-makeExpression op@Subtract left right = 
-   if rv >= lv || rv*2 == lv then Nothing else Just (ArithmeticExpression left op right)
-   where lv = value left
-         rv = value right
-makeExpression op@Multiply left right = 
-   if rv == 1 then Nothing else Just (ArithmeticExpression left op right)
-   where rv = value right
-makeExpression op@Divide left right = 
-   if rv == 1 || (lv `mod` rv) /= 0 || rv^2 == lv then Nothing else Just (ArithmeticExpression left op right)
-   where lv = value left
-         rv = value right
+makeExpression op@Subtract left right =
+  if rv >= lv || rv * 2 == lv then Nothing else Just (ArithmeticExpression left op right)
+  where
+    lv = value left
+    rv = value right
+makeExpression op@Multiply left right =
+  if rv == 1 then Nothing else Just (ArithmeticExpression left op right)
+  where
+    rv = value right
+makeExpression op@Divide left right =
+  if rv == 1 || lv `mod` rv /= 0 || rv ^ 2 == lv then Nothing else Just (ArithmeticExpression left op right)
+  where
+    lv = value left
+    rv = value right
 
 combinerUsing :: Operation -> Expression -> Maybe (Expression -> Maybe Expression)
 combinerUsing op@Add left = Just (makeExpression op left)
-combinerUsing op@Subtract left = 
-   if lv < 3 then Nothing else Just (makeExpression op left)
-   where lv = value left
-combinerUsing op left = 
-   if lv == 1 then Nothing else Just (makeExpression op left)
-   where lv = value left
-
+combinerUsing op@Subtract left =
+  if lv < 3 then Nothing else Just (makeExpression op left)
+  where
+    lv = value left
+combinerUsing op left =
+  if lv == 1 then Nothing else Just (makeExpression op left)
+  where
+    lv = value left
 
 class Nillable a where
-   nil :: a
-   
+  nil :: a
+
 instance Nillable (Maybe a) where
-   nil = Nothing
+  nil = Nothing
 
 parallelFold :: Nillable a => (a -> a -> a) -> [a] -> a
 parallelFold _ [] = nil
 parallelFold f xs = par lf $ f lf rf
-   where (left, right) = splitAt 1000 xs
-         lf = foldr f nil left
-         rf = parallelFold f right
+  where
+    (left, right) = splitAt 1000 xs
+    lf = foldr f nil left
+    rf = parallelFold f right
 
 findBest :: Int -> Maybe Expression -> Maybe Expression -> Maybe Expression
 findBest _ Nothing Nothing = Nothing
 findBest target j@(Just e) Nothing = if differenceFrom target e > 10 then Nothing else j
 findBest target Nothing j@(Just e) = findBest target j Nothing
 findBest target j1@(Just e1) j2@(Just e2)
-   | min diff1 diff2 > 10 = Nothing
-   | diff1 < diff2 = j1
-   | diff1 > diff2 = j2
-   | count e1 <= count e2 = j1
-   | otherwise = j2
-   where diff1 = differenceFrom target e1
-         diff2 = differenceFrom target e2
+  | min diff1 diff2 > 10 = Nothing
+  | diff1 < diff2 = j1
+  | diff1 > diff2 = j2
+  | count e1 <= count e2 = j1
+  | otherwise = j2
+  where
+    diff1 = differenceFrom target e1
+    diff2 = differenceFrom target e2
 
 differenceFrom :: Int -> Expression -> Int
-differenceFrom target expr = abs (target - (value expr))
-
+differenceFrom target expr = abs (target - value expr)
 
 data Priority = Low | High | Atomic deriving (Eq, Ord)
 
 class Prioritizable a where
-   priority :: a -> Priority
-
+  priority :: a -> Priority
 
 data Operation = Add | Subtract | Multiply | Divide deriving (Show, Enum)
 
@@ -113,15 +118,14 @@ eval Multiply a b = value a * value b
 eval Divide a b = value a `div` value b
 
 instance Prioritizable Operation where
-   priority Add = Low
-   priority Subtract = Low
-   priority _ = High
+  priority Add = Low
+  priority Subtract = Low
+  priority _ = High
 
 commutative :: Operation -> Bool
 commutative Add = True
 commutative Multiply = True
 commutative _ = False
-
 
 data Expression = NumberExpression Int | ArithmeticExpression Expression Operation Expression
 
@@ -135,24 +139,26 @@ count (ArithmeticExpression left _ right) = sum [count left, count right]
 
 numbersUsed :: Expression -> [Int]
 numbersUsed (NumberExpression n) = [n]
-numbersUsed (ArithmeticExpression left _ right) = concat [numbersUsed left, numbersUsed right]
+numbersUsed (ArithmeticExpression left _ right) = numbersUsed left ++ numbersUsed right
 
 instance Eq Expression where
-   a == b = value a == value b
-   
+  a == b = value a == value b
+
 instance Prioritizable Expression where
-   priority (NumberExpression _) = Atomic
-   priority (ArithmeticExpression _ op _) = priority op
-   
+  priority (NumberExpression _) = Atomic
+  priority (ArithmeticExpression _ op _) = priority op
+
 instance Show Expression where
-   show (NumberExpression n) = show n
-   show (ArithmeticExpression left op right) = 
-      join " " [
-         parensIf (priority left < priority op) (show left), 
-         symbol op, 
-         parensIf (priority op > priority right || (priority op == priority right && not(commutative op)))
-            (show right)
-         ]
+  show (NumberExpression n) = show n
+  show (ArithmeticExpression left op right) =
+    join
+      " "
+      [ parensIf (priority left < priority op) (show left),
+        symbol op,
+        parensIf
+          (priority op > priority right || priority op == priority right && not (commutative op))
+          (show right)
+      ]
 
 parensIf :: Bool -> String -> String
 parensIf False s = s
