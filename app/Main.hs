4421a1563d7dffd5e8a1c0f7864ccbe802462776
Jeremy Hicks <jerhicks@synamedia.com>
2022-01-21 11:45:49 +0000

    Refactor

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Main where
 
 import Countdown
@@ -9,35 +10,39 @@ import System.Random
 main = do
    args <- getArgs
    rand <- getStdGen
-   case validateAndConvert args rand of
-      (Message m) -> putStrLn m
-      (Numbers n) -> solveIt n
+   case validateArgs args of
+      (Just m) -> putStrLn m
+      _ -> case validateNumbers nums of
+              (Just m) -> putStrLn m
+              _ -> solveIt $ normalise nums rand
+         where nums = [read a::Int | a <- args]
 
-solveIt (target:numbers)  = do
+normalise :: RandomGen a => [Int] -> a -> [Int]
+normalise [bigNums] rand = randomNumbers rand bigNums
+normalise nums _ = nums
+
+solveIt :: [Int] -> IO ()
+solveIt (target:numbers) = do
     putStrLn (join " " ["Solving with target number:", show target, "and source numbers:", show numbers])
-    putStrLn (printOut (solve target numbers))
+    case solve target numbers of
+        (Just e) -> putStrLn $ join " = " [show e, show (value e)]
+        _ -> putStrLn "No solution found"
 
-printOut :: Maybe Expression -> String
-printOut Nothing = "No solution found"
-printOut (Just e) = join " = " [show e, show (value e)]
+validateArgs :: [String] -> Maybe String
+validateArgs [] = Just "Must specify at least one argument"
+validateArgs xs
+   | all (all isDigit) xs = Nothing
+   | otherwise = Just "All arguments must be numeric"
 
-data ValidationResult = Message String | Numbers [Int]
+validateNumbers :: [Int] -> Maybe String
+validateNumbers (x:[])
+   | x < 0 || x > 4 = Just "Number of big numbers must be in the range 0 to 4"
+   | otherwise = Nothing
+validateNumbers nums@(target:numbers)
+   | (not.validTarget) target = Just "Target must be in the range 100 to 999 inclusive"
+   | (not.validNumbers) numbers = Just "Numbers must include up to 2 each of the numbers 1 to 10 and up to 1 each of 25, 50, 75 and 100"
+   | otherwise = Nothing
 
-validateAndConvert :: RandomGen a => [String] -> a -> ValidationResult
-validateAndConvert [] _ = Message "Must specify at least one argument"
-validateAndConvert args rand
-   | or [any (not.isDigit) x | x <- args] = Message "All arguments must be numeric"
-   | otherwise = validateNumbers [read x::Int | x <- args] rand
-   
-validateNumbers :: RandomGen a => [Int] -> a -> ValidationResult
-validateNumbers (x:[]) rand
-   | x < 0 || x > 4 = Message "Number of big numbers must be in the range 0 to 4"
-   | otherwise = Numbers $ randomNumbers rand x
-validateNumbers nums@(target:numbers) _
-   | (not.validTarget) target = Message "Target must be in the range 100 to 999 inclusive"
-   | (not.validNumbers) numbers = Message "Numbers must include up to 2 each of the numbers 1 to 10 and up to 1 each of 25, 50, 75 and 100"
-   | otherwise = Numbers nums
-   
 validTarget :: Int -> Bool
 validTarget n = n >= 100 && n <= 999
 
@@ -51,7 +56,7 @@ validNumber n ns
    | n <= 10 = occurrences n ns <= 2
    | n `mod` 25 == 0 = occurrences n ns == 1
    | otherwise = False
-   
+
 randomNumbers :: RandomGen a => a -> Int -> [Int]
 randomNumbers rand bigOnes = concat [take 1 (randomRs (100, 999) rand),
                                      take bigOnes (randomFrom rand bigNumbers),
