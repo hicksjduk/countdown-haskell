58d1338a8a46392bc0710d38f064bf688375be99
Jeremy Hicks <jerhicks@synamedia.com>
2022-02-03 11:49:11 +0000

    Refactoring

--- app/Main.hs
+++ app/Main.hs
@@ -12,39 +12,31 @@ import Utils
 main = do
   args <- getArgs
   rand <- getStdGen
-  case validateArgs args of
-    (Just m) -> putStrLn m
-    _ -> case validateNumbers nums of
-      (Just m) -> putStrLn m
-      _ -> solveIt $ normalise nums rand
-      where
-        nums = [read a :: Int | a <- args]
+  either putStrLn solveIt $ do
+    numArgs <- argsToNumeric args
+    numericToNumbers rand numArgs
 
-normalise :: RandomGen a => [Int] -> a -> [Int]
-normalise [bigNums] rand = randomNumbers rand bigNums
-normalise nums _ = nums
+argsToNumeric :: [String] -> Either String [Int]
+argsToNumeric [] = Left "Must specify at least one argument"
+argsToNumeric xs
+  | all (all isDigit) xs = Right [read x::Int | x <- xs]
+  | otherwise = Left "All arguments must be numeric"
+
+numericToNumbers :: RandomGen a => a -> [Int] -> Either String [Int]
+numericToNumbers rand [x]
+  | x < 0 || x > 4 = Left "Number of big numbers must be in the range 0 to 4"
+  | otherwise = Right $ randomNumbers rand x
+numericToNumbers _ nums@(target : numbers)
+  | (not . validTarget) target = Left "Target must be in the range 100 to 999 inclusive"
+  | (not . validNumbers) numbers = Left "Numbers must include up to 2 each of the numbers 1 to 10 and up to 1 each of 25, 50, 75 and 100"
+  | otherwise = Right nums
 
 solveIt :: [Int] -> IO ()
 solveIt (target : numbers) = do
   putStrLn $ unwords ["Solving with target number:", show target, "and source numbers:", show numbers]
   case solve target numbers of
+    Nothing -> putStrLn "No solution found"
     (Just e) -> putStrLn $ unwords [show e, "=", show (value e)]
-    _ -> putStrLn "No solution found"
-
-validateArgs :: [String] -> Maybe String
-validateArgs [] = Just "Must specify at least one argument"
-validateArgs xs
-  | all (all isDigit) xs = Nothing
-  | otherwise = Just "All arguments must be numeric"
-
-validateNumbers :: [Int] -> Maybe String
-validateNumbers [x]
-  | x < 0 || x > 4 = Just "Number of big numbers must be in the range 0 to 4"
-  | otherwise = Nothing
-validateNumbers nums@(target : numbers)
-  | (not . validTarget) target = Just "Target must be in the range 100 to 999 inclusive"
-  | (not . validNumbers) numbers = Just "Numbers must include up to 2 each of the numbers 1 to 10 and up to 1 each of 25, 50, 75 and 100"
-  | otherwise = Nothing
 
 validTarget :: Int -> Bool
 validTarget n = n >= 100 && n <= 999
@@ -55,10 +47,10 @@ validNumbers ns = all (`validNumber` ns) $ distinct ns
 validNumber :: Int -> [Int] -> Bool
 validNumber n ns
   | n < 1 = False
-  | n > 100 = False
   | n <= 10 = occurrences n ns <= 2
-  | n `mod` 25 == 0 = occurrences n ns == 1
-  | otherwise = False
+  | n > 100 = False
+  | n `mod` 25 /= 0 = False
+  | otherwise = occurrences n ns == 1
 
 randomNumbers :: RandomGen a => a -> Int -> [Int]
 randomNumbers rand bigOnes =
